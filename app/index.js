be8f7517fa3be573426d4b07e1042087bc76abc5
zhenfeng-zhu <codeless98@163.com>
2018-05-06 14:58:08 +0800

    :construction: add cluster

--- app/index.js
+++ app/index.js
@@ -1,6 +1,19 @@
-require('dotenv').config();
-const server = require('./server');
+require("dotenv").config();
+const cluster = require("cluster");
+const numCPUs = require("os").cpus().length;
+const server = require("./server");
 
 const port = process.env.PORT || 3000;
 
-server.listen(port, () => console.log(`API server started on ${port}`));
+if (cluster.isMaster) {
+  console.log(`master ${process.pid} is running`);
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
+  cluster.on("exit", (worker, code, signal) => {
+    console.log(`worker ${worker.process.pid} died`);
+  });
+} else {
+  server.listen(port, () => console.log(`API server started on ${port}`));
+  console.log(`worker ${process.pid} started`);
+}
